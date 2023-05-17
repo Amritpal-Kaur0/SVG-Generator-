5e54d366a77629f1b5dc21d85c7895940450c81e
Amrit <128442182+Amritpal-Kaur0@users.noreply.github.com>
2023-05-17 22:05:46 +0530

    WriteFile link set

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ inquirer.prompt(questions).then((data) => {
         const squareX = 98;
         const squareY = 105;
       
-        const squareLogoSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200" style="fill:${bg}">
+       const squareLogoSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200" style="fill:${bg}">
           ${squareSvg}
           <text x="${squareX}" y="${squareY}" fill="${textColor}" font-size="30" text-anchor="middle">${text}</text>  
         </svg> `;
